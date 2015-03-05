37a901ec4983b6285788c2c14fc385d7f448cc60
Spring.Zhou <spring.zhou@samesamechina.com>
2015-03-05 19:36:25 +0800

    表单页以及分享页

--- js/main.js
+++ js/main.js
@@ -63,4 +63,198 @@ function deviceMotionHandler(eventData) {
 
 $(document).on("pageinit","#index",function(){  
     init();
-});   
+}); 
+
+
+
+var errtxt = {
+            "name" : "请输入您的姓名！",
+            "mobile" : "请输入您的手机号！",
+            "check" : "请填写正确手机号！",
+            "address" : "请输入您的地址！",
+            
+
+        }
+function submitform(){
+    var name = $("#name").val();
+    var mobile = $("#mobile").val();
+    var address = $("#address").val();
+
+    if(name==""){
+        $("#name").addClass("errorTips");
+        $("#name").val("").attr("placeholder",errtxt.name);
+        return false;
+
+    }
+    if(address==""){
+        $("#address").addClass("errorTips");
+        $("#address").val("").attr("placeholder",errtxt.address);
+        return false;
+    }
+    if(mobile==""){
+        $("#mobile").addClass("errorTips");
+        $("#mobile").val("").attr("placeholder",errtxt.mobile);
+        return false;
+    }
+    if(!mobile.match(/13[0-9]{9}|14[0-9]{9}|15[0-9]{9}|18[0-9]{9}/)){
+        $("#mobile").addClass("errorTips");
+        $("#mobile").val("").attr("placeholder",errtxt.check);
+        return false;
+    }
+    
+        //$( ".tips" ).css( 'display', 'block');
+    $.ajax({
+        type: "POST",
+        url: "/Request.php?model=finish",
+        data: {
+          "name": name,
+          "mobile":mobile,
+          "address":address
+        },
+        dataType:"json",
+        success: function(data){
+           if(data.code==1){
+           		$(".success_tips").show();
+                
+           }else{
+                alert(data.msg);
+           }
+           //window.location.href="question.html"
+
+        }
+      }); 
+}
+
+
+
+
+/* ----------- 分享js ----------- */
+
+$.ajax({  
+        type: "GET",  
+        url: "/access_token/access_token.php", //orderModifyStatus 
+        data:{url:window.location.href},
+        dataType:"json",  
+        async:false,  
+        cache:false,  
+        success: function(data){  
+       wechatShare(data.time,data.sign);
+        },  
+        error: function(json){  
+            //alert("数据获取异常，请刷新后重试...");  
+        }  
+ });  
+
+
+
+function wechatShare(timestamp_val,signature_val){
+  
+  var SHARE_TITLE = 'lancaster摇一摇';
+  var SHARE_LINK = 'http://lancasterld.samesamechina.com';
+  var SHARE_IMG = 'http://lancasterld.samesamechina.com/images/logo.jpg';
+  var SHARE_DESC = 'lancaster摇一摇新产品';
+  wx.config({
+      debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      appId: 'wx77e36122ac152d47', // 必填，公众号的唯一标识
+      timestamp: timestamp_val, // 必填，生成签名的时间戳
+      nonceStr: 'asdkhaedhqwui', // 必填，生成签名的随机串
+      signature: signature_val,// 必填，签名，见附录1
+      jsApiList: [
+        'checkJsApi',
+        'onMenuShareTimeline',
+        'onMenuShareAppMessage',
+        'onMenuShareQQ',
+        'onMenuShareWeibo',
+        'hideMenuItems',
+        'showMenuItems',
+        'hideAllNonBaseMenuItem',
+        'showAllNonBaseMenuItem',
+        'translateVoice',
+        'startRecord',
+        'stopRecord',
+        'onRecordEnd',
+        'playVoice',
+        'pauseVoice',
+        'stopVoice',
+        'uploadVoice',
+        'downloadVoice',
+        'chooseImage',
+        'previewImage',
+        'uploadImage',
+        'downloadImage',
+        'getNetworkType',
+        'openLocation',
+        'getLocation',
+        'hideOptionMenu',
+        'showOptionMenu',
+        'closeWindow',
+        'scanQRCode',
+        'chooseWXPay',
+        'openProductSpecificView',
+        'addCard',
+        'chooseCard',
+        'openCard'
+      ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+  });
+
+  wx.ready(function(){
+    
+    // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+    wx.onMenuShareTimeline({
+        title: SHARE_TITLE, // 分享标题
+        link: SHARE_LINK, // 分享链接
+        imgUrl: SHARE_IMG, // 分享图标
+        success: function () {
+            // 用户确认分享后执行的回调函数
+            //alert('分享成功')
+        },
+        cancel: function () { 
+            // 用户取消分享后执行的回调函数
+            // alert("分享失败")
+        }
+    });
+    
+    
+    wx.onMenuShareAppMessage({
+        title: SHARE_TITLE, // 分享标题
+        link: SHARE_LINK, // 分享链接
+        imgUrl: SHARE_IMG, // 分享图标
+      desc: SHARE_DESC,
+        success: function () { 
+            // 用户确认分享后执行的回调函数
+            //alert('分享成功')
+        },
+        cancel: function () { 
+            // 用户取消分享后执行的回调函数
+           // alert("分享失败")
+        }
+    });
+      
+  });
+
+  wx.error(function(res){
+    //alert("无法使用微信JS")
+      // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+
+  });
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
