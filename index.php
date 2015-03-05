7290fc055b722413b979cc67a120f11b231e0083
demonzhang <demon.zhang@samesamechina.com>
2015-03-05 16:58:09 +0800

    中转页面

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+include_once('./config/database.php');
+include_once('./config/Pdb.php');
+if(!isset($_SESSION["openid"])){
+	header("Location: http://lancaster.ifm-paris.cn/Wechat/oauth2?callback=http://lancasterld.samesamechina.com/");
+	exit;
+}
+$db=Pdb::getDb();
+$sql="select id from same_weixin_user where openid=".$db->quote($_SESSION["openid"]);
+$rs=$db->getOne($sql);
+if($rs){
+	header("Location: http://lancasterld.samesamechina.com/home.html#index");
+	exit;
+}
+header("Location: http://lancasterld.samesamechina.com/home.html#attention");
+exit;
+?>
