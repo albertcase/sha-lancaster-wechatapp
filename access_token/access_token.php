9e43a027ab16d2c9aed447f340aadfd2e0c86412
demonzhang <demon.zhang@samesamechina.com>
2015-03-16 14:25:51 +0800

    access_token

--- access_token/access_token.php
+++ access_token/access_token.php
@@ -7,28 +7,34 @@ print_r($token);
 */
 $url=isset($_REQUEST['url'])?$_REQUEST['url']:"http://lancasterld.samesamechina.com";
 $time=file_get_contents("time.txt");
-$sign=file_get_contents("access_token.txt");
-if(time()-$time>=10){
+$access_token=file_get_contents("access_token.txt");
+$ticket=file_get_contents("ticket.txt");
+if(time()-$time>=100){
 	//token过期重新获取
 	$appid="wx77e36122ac152d47";
 	$secret="406d2bd1165654ed9225370faf499dea";
 	$token=file_get_contents("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$appid."&secret=".$secret);
 	$token=json_decode($token,true);
 	$access_token=$token['access_token'];
+	//获取ticket
+	$ticketfile=file_get_contents("https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=".$access_token."&type=jsapi");
+	$ticketfile=json_decode($ticketfile,true);
+	$ticket=$ticketfile['ticket'];
 	$time=time();
 	$fp = fopen("time.txt", "w");
 	fwrite($fp,$time);
 	fclose($fp);
-	$ticketfile=file_get_contents("https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=".$access_token."&type=jsapi");
-	$ticketfile=json_decode($ticketfile,true);
-	$ticket=$ticketfile['ticket'];
-	$ticketstr="jsapi_ticket=".$ticket."&noncestr=asdkhaedhqwui&timestamp=".$time."&url=".$url;
-	$sign=sha1($ticketstr);
 	$fp = fopen("access_token.txt", "w");
-	fwrite($fp,$sign);
+	fwrite($fp,$access_token);
+	fclose($fp);
+	$fp = fopen("ticket.txt", "w");
+	fwrite($fp,$ticket);
 	fclose($fp);
 }
-echo json_encode(array("time"=>$time,"sign"=>$sign));
+$time=time();
+$ticketstr="jsapi_ticket=".$ticket."&noncestr=asdkhaedhqwui&timestamp=".$time."&url=".$url;
+$sign=sha1($ticketstr);
+echo json_encode(array("time"=>$time."","access_token"=>$access_token,"sign"=>$sign));
 exit();
 
 ?>
